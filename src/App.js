685d027486330f290721a091ca6b0394c4555f14
Marius de Corte <mdecorte@mirabeau.nl>
2018-08-30 15:46:26 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import open from './open.png'
+import './App.css'
+
+class App extends Component {
+  state = {
+    active: false,
+  }
+
+  handleClick = () => {
+    if (this.state.active) return
+    this.setState({ active: true })
+    window.setTimeout(() => {
+      this.setState({ active: false })
+    }, 2000)
+  }
+
+  render() {
+    const className = this.state.active ? 'open-sign clicked' : 'open-sign'
+    return (
+      <main className="main">
+        <img
+          src={open}
+          className={className}
+          alt="logo"
+          onClick={this.handleClick}
+        />
+      </main>
+    )
+  }
+}
+
+export default App
